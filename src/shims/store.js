b591e7716c4554ebcd70aae477d1620609ec092f
Luiz Kim <luiz.kim@controleonline.com>
2026-07-10 16:49:43 -0300

    Corrige o caminho de importação do módulo de testes no store.js

--- src/shims/store.js
+++ src/shims/store.js
@@ -1,4 +1,4 @@
-import tests from '@controleonline/ui-tests/src/store/tests';
+import tests from '../../node_modules/@controleonline/ui-tests/src/store/tests.js';
 
 // Store map consumed by the shared @store runtime.
 export default {
